633e9ff03b1fd8d679898ec4dd3c6f4475786871
Sam Singer <samuel.singer1@gmail.com>
2018-02-12 13:48:04 -0700

    Post new item working

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,7 @@
 const showAllItems = (allItems) => {
   allItems.forEach(item => {
     $('.items-container').append(`
-      <div> class='item${item.id} item'>
+      <div> id=${item.id} class='item${item.id}' item ${item.cleanliness}'>
         <h2 class='item-name'>${item.name}</h2>
       </div>`);
   });
@@ -15,4 +15,33 @@ const getAllItems = () => {
     });
 };
 
+const clearInputs = () => {
+  $('.name-input').val('');
+  $('.reason-input').val('');
+};
+
+const postItem = () => {
+  const newItem = {
+    name: $('.name-input').val(),
+    reason: $('.reason-input').val(),
+    cleanliness: $('.drop-down').val()
+  };
+
+  fetch('/api/v1/items', {
+    method: 'POST',
+    body: JSON.stringify(newItem),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(items => {
+      showAllItems(items);
+      clearInputs();
+    })
+    .catch(error => console.log(error));
+};
+
+
 $(document).ready(getAllItems);
+$('.submit-button').on('click', postItem);
