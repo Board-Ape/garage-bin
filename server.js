3a72c6b70f66f10ebc6f663a5dbeeb2807faf3d9
Sam Singer <samuel.singer1@gmail.com>
2018-02-12 11:08:53 -0700

    PUT is opertaional

--- server.js
+++ server.js
@@ -34,6 +34,24 @@ app.get('/api/v1/items', (request, response) => {
     });
 });
 
+app.post('/api/v1/items', (request, response) => {
+  const item = request.body;
+
+  for (let requiredParameter of ['name', 'reason', 'cleanliness']) {
+    if (!item[requiredParameter]) {
+      return response.status(422).send({ error: `You're missing a ${requiredParameter}.` });
+    }
+  }
+
+  database('items').insert(item, '*')
+    .then(item => {
+      response.status(201).json(item);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
