5b95403a5eee89e373afba236a8423f2bf6c3b6b
Sam Singer <samuel.singer1@gmail.com>
2018-02-12 11:47:34 -0700

    Intial tests passing

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -9,9 +9,68 @@ const database = require('knex')(configuration);
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-
+  it('should return the homepage', () => {
+    return chai.request(server)
+      .get('/')
+      .then(response => {
+        response.should.have.status(200);
+        response.should.be.html;
+      })
+      .catch(error => {
+        throw error;
+      });
+  });
+  it('should return a 404 for a route that does not exist', () => {
+    return chai.request(server)
+      .get('/unhappy')
+      .then(response => {
+        response.should.have.status(404);
+      })
+      .catch(error => {
+        throw error;
+      });
+  });
 });
 
 describe('API Routes', () => {
+  before((done) => {
+    database.migrate.latest()
+      .then( () => done())
+      .catch(error => {
+        throw error;
+      });
+  });
 
+  beforeEach((done) => {
+    database.seed.run()
+      .then(() => done())
+      .catch(error => {
+        throw error;
+      });
+  });
+
+  describe('GET /api/v1/items', () => {
+    it('should get items from database', (done) => {
+      chai.request(server)
+        .get('/api/v1/items')
+        .then(response => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(3);
+          response.body[0].should.have.property('id');
+          response.body[0].id.should.equal(1);
+          response.body[0].should.have.property('name');
+          response.body[0].should.have.property('reason');
+          response.body[0].should.have.property('cleanliness');
+          response.body[0].name.should.equal('hockey pads');
+          response.body[0].reason.should.equal('fast all the way');
+          response.body[0].cleanliness.should.equal('Rancid');
+          done();
+        })
+        .catch(error => {
+          throw error;
+        });
+    });
+  });
 });
