1e38fca11557dacd41f55bea8d139b0f1f76dbd8
Sam Singer <samuel.singer1@gmail.com>
2018-02-12 10:24:46 -0700

    HTTPS redirect

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+const bodyParser = require('body-parser');
+
+const requireHTTPS = (request, response, next) => {
+  if (request.header('x-forwarded-proto') !== 'https'){
+    response.redirect(`https://${request.header('host')}${request.url}`);
+  }
+  next();
+};
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(requireHTTPS);
+}
+
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Garage Bin';
 
